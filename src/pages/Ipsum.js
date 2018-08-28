5c10988981d7ee4a6f6633e62962ef2cfcf830b2
Eduardo <root@eduardo-testbed.cluster>
2018-08-28 15:53:18 -0400

    Add Wizard

--- src/pages/Ipsum.js
+++ src/pages/Ipsum.js
@@ -1,7 +1,112 @@
 import * as React from 'react';
 import { Alert, Grid } from 'patternfly-react';
 
+import { Wizard, Button } from 'patternfly-react';
+
 const logo = require('../logo.svg');
+
+const wizardSteps = [
+  {
+    title: 'General',
+    render: () => <p>(Step 1 Contents Here)</p>
+  },
+  {
+    title: 'Details',
+    render: () => <p>(Step 2 Contents Here)</p>
+  },
+  {
+    title: 'Results',
+    render: () => <p>(Step 3 Contents Here)</p>
+  }
+];
+
+const isLastStep = (step /* number */) => wizardSteps.length -1 ===  step;
+
+export class WizardPatternExample extends React.Component {
+  state = {
+    showModal: false,
+    loading: false,
+    activeStepIndex: 0 // eslint-disable-line no-unused-state
+  };
+
+  close = () => { this.setState({ showModal: false, loading: false }); };
+  hide = () => { this.close(); console.log('Hiding ...'); }
+
+  exit = () => { 
+    this.close();
+    console.log('Closing ..');
+    if (isLastStep(this.state.activeStepIndex)) {
+      console.log('Final step.')
+    }
+  }
+
+  onStepChanged = (index) => {
+    console.log('Step', isLastStep(index) ? 'final' : index);
+    this.setState({ activeStepIndex: index });
+  }
+
+  open = () => {
+    console.log('Open ...');
+    this.setState({ showModal: true, loading: true });
+    setTimeout(() => {
+      this.setState({ loading: false });
+    }, 1000);
+  };
+
+  renderStateless = () => {
+    // We need knobs on the stateless example, because we must drive its state ourselves.
+    const { loading, activeStepIndex } = this.state;
+
+    return (
+      <div>
+        <Button bsStyle="primary" bsSize="large" onClick={this.open}>
+          Launch Stateless Wizard
+        </Button>
+
+        <Wizard.Pattern
+          show={this.state.showModal}
+          onHide={this.hide}
+          onExited={this.exit}
+
+          title="Stateless Wizard Pattern Example"
+          nextStepDisabled={false}
+          steps={wizardSteps}
+          loadingTitle="Loading..."
+          loadingMessage="This may take a minute."
+          onStepChanged={this.onStepChanged}
+          loading={this.state.loading}
+          activeStepIndex={activeStepIndex}
+        />
+      </div>
+    );
+  };
+  renderStateful = () => {
+    // No knobs for the stateful example, we want to let it control its own state.
+    return (
+      <div>
+        <Button bsStyle="primary" bsSize="large" onClick={this.open}>
+          Launch Stateful Wizard
+        </Button>
+
+        <Wizard.Pattern.Stateful
+          show={this.state.showModal}
+          onHide={this.hide}
+          onExited={this.exit}
+          title="Stateful Wizard Pattern Example"
+          shouldDisableNextStep={activeStepIndex => false}
+          steps={wizardSteps}
+          loadingTitle="Loading..."
+          loadingMessage="This may take a minute."
+          loading={this.state.loading}
+          closeText='finish'
+        />
+      </div>
+    );
+  };
+  render() {
+    return this.props.stateful ? this.renderStateful() : this.renderStateless();
+  }
+}
 
 interface State {
   alertVisible: boolean;
@@ -23,24 +128,8 @@ class IpsumPage extends React.Component<any, State> {
   render() {
     return (
       <Grid fluid className="container-pf-nav-pf-vertical">
-        <div className="page-header">
-          <h2>Overview</h2>
-        </div>
-        <div className="App-body">
-          {this.state.alertVisible && (
-            <Alert type="success" onDismiss={this.dismissAlert}>
-              <span>Well done! You&apos;ve installed this demo correctly.</span>
-            </Alert>
-          )}
-          <div className="App-intro">
-            <img src={logo} className="App-logo" alt="logo" />
-            <h2>Welcome to Patternfly React</h2>
-          </div>
-          <p className="App-paragraph">
-            To get started, edit <code>src/App.js</code> and save to reload.
-          </p>
-        </div>
-      </Grid>
+				<WizardPatternExample stateful={false}/>
+			</Grid>
     );
   }
 }
